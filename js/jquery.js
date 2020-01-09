6a5ebefa48fa24937ffa93b31e472aacc8f9ad94
falcon-811 <almlekh.811@gmail.com>
2020-01-09 03:53:25 +0300

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#button1").click(function(){
+      $("#p1").toggle("fast");
+    });
+
+    $("#button2").click(function(){
+      $("#p2").toggle("fast");
+    });
+
+    $("#button3").click(function(){
+      $("#p3").toggle("fast");
+    });
+      
+      
+      
+    
+  });
